c4cee6d1216d52db47b454d39c0b0338180e6f57
Tebyt <chenpan5532323@gmail.com>
2016-03-04 20:59:09 -0500

    api finished

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
+var api = require('./routes/api');
 
 var app = express();
+
+var http = require('http').Server(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,13 +26,100 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
+app.use('/api', api);
+
+
+
+
+// socket.io
+// var io = require('socket.io')(http);
+
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
+// var send = true;
+// io.on('connection', function (socket) {
+//     console.log("connected");
+//     socket.on('chat message', function (msg) {
+//         console.log(msg);
+//         if (send) {
+//             io.emit('chat message', msg);
+//             send = false;
+//         }
+//     });
+// });
+
+// http.listen(3000, function () {
+//     console.log('listening on *:3000');
+// });
+
+// Twitter-Stream
+
+var Twit = require('twit')
+
+var T = new Twit({
+    consumer_key: 'NEcFV7WwVNlgt2PGzcLAd9yHs',
+    consumer_secret: 'yrDRbggUfbWOkjHYDYHsfckBy9FLKX8f43340WVHgaTterodMJ',
+    access_token: '545857883-mIGQyyOKbK0gLmjLE1WWchK4PfpaRittFiBb5irj',
+    access_token_secret: 'dr0pOppvO8mONnRozgFDGTobuNfkQeYJKGJ6yx9Nn8Ega'
+})
+
+// getting stream from NYC
+var stream = T.stream('statuses/filter', { locations: "-74,40,-73,41" })
+// var stream = T.stream('statuses/sample')
+
+stream.on('tweet', function (tweet) {
+    if (tweet.geo != null) {
+        // console.log(tweet.geo);
+        sendToDB(tweet);
+    }
+    // io.emit('chat message', tweet);
+})
+
+
+// Send to ElasticSearch
+function sendToDB(tweet) {
+    
+    var http = require('http');
+    var data = JSON.stringify(tweet);
+
+    // An object of options to indicate where to post to
+    var post_options = {
+        // host: '127.0.0.1',
+        // port: '9200',
+        hostname: 'dori-us-east-1.searchly.com',
+        path: '/twitter/' + tweet.id_str,
+        method: 'POST',
+        auth: 'paas:8fa0549c7855701ee173a9dbe37cbfd3'
+        // headers: {
+        //     'Content-Type': 'application/json',
+        //     'Content-Length': Buffer.byteLength(data)
+        // }
+    };
+
+    // Set up the request
+    var post_req = http.request(post_options, function (res) {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    }).on('error', function(e){
+    console.log(e)
+  });
+    // post the data
+    post_req.write(data);
+    post_req.end();
+}
+
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,84 +127,24 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
-// Twitter-Map
-
-var Twit = require('twit')
-
-var T = new Twit({
-  consumer_key:         'NEcFV7WwVNlgt2PGzcLAd9yHs',
-  consumer_secret:      'yrDRbggUfbWOkjHYDYHsfckBy9FLKX8f43340WVHgaTterodMJ',
-  access_token:         '545857883-mIGQyyOKbK0gLmjLE1WWchK4PfpaRittFiBb5irj',
-  access_token_secret:  'dr0pOppvO8mONnRozgFDGTobuNfkQeYJKGJ6yx9Nn8Ega'
-})
-
-var stream = T.stream('statuses/sample')
-
-stream.on('tweet', function (tweet) {
-  console.log(tweet)
-})
-
-
-
-// var Twitter = require('twitter');
-
-// var client = new Twitter({
-//   consumer_key: "mkb4SJfnUDEtP5DuqdWl3sOWu",
-//   consumer_secret: "JeYMNYqoQzX8kiHC33iTklgLcQ9upU2ftDvrKtFCrJFG8Cy8Th",
-//   access_token_key: "545857883-RV7drTvRsUVygHIPGto7ozhwNd54GTs3xSmOhQDa",
-//   access_token_secret: "16w3YvbvCjBxftVdVSncYLp9PmmnewlwCpr7Nw9oodrKi"
-// });
-
-// /**
-//  * Stream statuses filtered by keyword
-//  * number of tweets per second depends on topic popularity
-//  **/
-// client.stream('statuses/sample', function(stream) {
-//   stream.on('message', function(tweet) {
-//     console.log(tweet.text);
-//   });
-
-//   stream.on('error', function(error) {
-//     throw error;
-//   });
-// });
-
-
-// ///////////
-
-// var tweets = require('tweets');
-
-// var stream = new tweets({
-//   consumer_key:         'NEcFV7WwVNlgt2PGzcLAd9yHs',
-//   consumer_secret:      'yrDRbggUfbWOkjHYDYHsfckBy9FLKX8f43340WVHgaTterodMJ',
-//   access_token:         '545857883-mIGQyyOKbK0gLmjLE1WWchK4PfpaRittFiBb5irj',
-//   access_token_secret:  'dr0pOppvO8mONnRozgFDGTobuNfkQeYJKGJ6yx9Nn8Ega'
-// });
-// stream.on('message', function(t){
-//     console.log("test");
-//   console.log("Got a tweet!", t);
-// });
-
-
 
 module.exports = app;
 
---
+++ routes/api.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+// ElasticSearch
+
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'paas:8fa0549c7855701ee173a9dbe37cbfd3@dori-us-east-1.searchly.com:80',
+    log: ['error', 'warning']
+});
+
+client.ping(function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+/* GET users listing. */
+router.get('/:toSearch', function (req, res, next) {
+
+    client.search({
+        index: 'twitter',
+        q: 'text:'+req.params.toSearch,
+    }, function (error, data) {
+        if (error) res.send(error);
+        console.log(data);
+        res.json(data.hits.hits);
+    });
+
+    
+});
+
+module.exports = router;
