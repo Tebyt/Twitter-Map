d429fadb3247d6a3ce2f2da0964097b7734589e3
Tebyt <chenpan5532323@gmail.com>
2016-03-06 14:23:09 -0500

    modularize and improve animation

--- public/javascripts/viz.js
+++ public/javascripts/viz.js
@@ -1,6 +1,5 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoidGVieXQiLCJhIjoiY2lsZmd0c3I0MXI4dHZubWMzdXdodGp6MyJ9.yHg2aSIkgkJHYgwCVpPiwg';
-var duration = 2000;
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v8',
@@ -11,28 +10,51 @@ var map = new mapboxgl.Map({
     zoom: 10.7
 
 });
-var id = 0;
-var marker_all;
-var marker_search;
-var marker_point;
-var marker_temp;
-function initMarkerTemp() {
-    marker_temp = {
-        "type": "FeatureCollection",
-        "features": []
-    };
+
+
+map.on('style.load', function () {
+    init();
+});
+function init() {
+    registerMarkers();
+    registerSocket();
+    fetchAllPoints();
 }
 
-var socket = io.connect();
-socket.on('tweet', function (data) {
-    console.log(data);
-    showPoint(data);
-});
-// var radius = 1;
-map.on('style.load', function () {
-    // Add marker data as a new GeoJSON source.
-    fetchAllPoints(registerMarker);
-});
+function registerMarkers() {
+    registerMarker("marker_all", "yellow");
+    registerMarker("marker_search", "yellow");
+    registerMarker("marker_temp", "yellow");
+}
+
+function registerMarker(name, color) {
+    window[name] = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+    map.addSource(name, {
+        "type": "geojson",
+        "data": window[name]
+    });
+    map.addLayer({
+        "id": name,
+        // "interactive": true,
+        "type": "circle",
+        "source": name,
+        "paint": {
+            "circle-color": color,
+            "circle-radius": 1,
+            "circle-opacity": 1
+        }
+    });
+}
+function registerSocket() {
+    socket = io.connect();
+    socket.on('tweet', function (data) {
+        console.log(data);
+        showPoint(data);
+    });
+}
 
 function filterData(key) {
     var filtered_data = marker_all.features.filter(function (d) {
@@ -43,14 +65,13 @@ function filterData(key) {
         "features": filtered_data
     }
     console.log(marker_search);
-    showFilteredPoint();
+    showFilteredPoints();
     // showTweets(filtered_data, key);
 }
 
-function showFilteredPoint() {
-    initMarkerTemp();
-    map.getSource("temp").setData(marker_temp);
-    map.getSource("search").setData(marker_search);
+function showFilteredPoints() {
+    emptyData("marker_tmp");
+    refreshData("marker_search");
     map.setLayoutProperty("all", 'visibility', 'none');
     map.setLayoutProperty("search", 'visibility', 'visible');
 }
@@ -78,133 +99,71 @@ function showTweet(tweet, key, div) {
     $(div).append(t);
 }
 
-function showAllPoint() {
-    initMarkerTemp();
-    map.getSource("temp").setData(marker_temp);
-    map.getSource("all").setData(marker_all);
-    map.setLayoutProperty("search", 'visibility', 'none');
-    map.setLayoutProperty("all", 'visibility', 'visible');
-    initMarkerTemp();
-}
-function registerMarker() {
-    map.addSource("all", {
-        "type": "geojson",
-        "data": marker_all
-    });
-    map.addSource("search", {
-        "type": "geojson",
-        "data": marker_search
-    });
-    initMarkerTemp();
-    map.addSource("temp", {
-        "type": "geojson",
-        "data": marker_temp
-    });
-
-    map.addLayer({
-        "id": "temp",
-        // "interactive": true,
-        "type": "circle",
-        "source": "temp",
-        "paint": {
-            "circle-color": "yellow",
-            "circle-radius": 1,
-            "circle-opacity": 1
-        }
-    });
-    map.addLayer({
-        "id": "all",
-        // "interactive": true,
-        "type": "circle",
-        "source": "all",
-        "paint": {
-            "circle-color": "yellow",
-            "circle-radius": 1,
-            "circle-opacity": 1
-        }
-    });
-    map.addLayer({
-        "id": "search",
-        // "interactive": true,
-        "type": "circle",
-        "source": "search",
-        "paint": {
-            "circle-color": "yellow",
-            "circle-radius": 1,
-            "circle-opacity": 1
-        }
-    });
-    showAllPoint();
+function showAllPoints() {
+    emptyData("marker_temp");
+    refreshData("marker_all");
+    map.setLayoutProperty("marker_search", 'visibility', 'none');
+    map.setLayoutProperty("marker_all", 'visibility', 'visible');
 }
 
-function fetchAllPoints(callback) {
+function emptyData(marker) {
+    window[marker].features = [];
+    refreshData(marker);
+}
+function refreshData(marker) {
+    map.getSource(marker).setData(window[marker]);
+}
+
+
+function fetchAllPoints() {
     console.log("fetching data");
     $.getJSON("/api/", function (data) {
         console.log("fetched");
-        marker_all = {
-            "type": "FeatureCollection",
-            "features": data
-        }
-        marker_search = marker_all;
-        marker_point = data[0];
-        callback();
-        // showPoint(marker_point);
-        // filterData("my");
+        marker_all.features = data;
+        showAllPoints();
     })
 }
 
+// For point animation
+var animation = [[1, 1], [3, 1], [20, 0.5], [200, 0]];
+var id = 0;
 var start_time;
+
 function showPoint(point) {
-    var cur_id = 'point' + id;
-    map.addSource(cur_id, {
-        "type": "geojson",
-        "data": point
-    });
-    map.addLayer({
-        "id": cur_id,
-        "source": cur_id,
-        "type": "circle",
-        "paint": {
-            "circle-radius": 1,
-            "circle-color": "lightblue",
-            "circle-opacity": 1
-        }
-    });
-    ++id;
     marker_all.features.push(point);
-    map.getSource("all").setData(marker_all);
     marker_temp.features.push(point);
-    map.getSource("temp").setData(marker_temp);
-    animatePoint(cur_id);
-}
-function animatePoint(id) {
-    // var cur_time = Date.now();
-    // var diff = cur_time - start_time;
-    // diff /= 1000;
-    // if (diff < 1) {
-    //     map.setPaintProperty("point", "circle-radius", diff * 1000);
-    //     map.setPaintProperty("point", "circle-opacity", 1 - diff);
-    //     requestAnimationFrame(animatePoint);
-    // } else {
-    // map.setLayoutProperty("point", 'visibility', 'none');
-    map.setLayoutProperty(id, 'visibility', 'visible');
-    map.setPaintProperty(id, "circle-radius", 200);
-    map.setPaintProperty(id, "circle-opacity", 0.01);
-    window.setTimeout(function () {
-        map.setLayoutProperty(id, 'visibility', 'none');
-        map.setPaintProperty(id, "circle-radius", 1);
-        map.setPaintProperty(id, "circle-opacity", 1);
-    }, duration)
-    // }
-    // var opacity = 1-radius/500;
-    // opacity = opacity<0? 0 : opacity;
-    // radius += 50;
-    // map.setPaintProperty("point", "circle-opacity", opacity);
-    // if (radius < 500) {
-    //     window.setTimeout(function () {
-    //         animatePoint(radius);
-    //     }, 100);
-    // } else {
+    refreshData("marker_temp");
     
-    // }
+    var cur_point = "marker_point"+id;
+    ++id;
+    registerMarker(cur_point, "lightblue");
+    window[cur_point].features = [point];
+    refreshData(cur_point);
+    start_time = Date.now();
+    animatePoint(cur_point);
+}
+// function animatePoint() {
+//     map.setLayoutProperty("marker_point", 'visibility', 'visible');
+//     map.setPaintProperty("marker_point", "circle-radius", 200);
+//     map.setPaintProperty("marker_point", "circle-opacity", 0.01);
+//     window.setTimeout(function () {
+//         map.setLayoutProperty("marker_point", 'visibility', 'none');
+//         map.setPaintProperty("marker_point", "circle-radius", 1);
+//         map.setPaintProperty("marker_point", "circle-opacity", 1);
+//     }, duration)
+// }
+
+function animatePoint(id) {
+    var cur_frame = Math.floor((Date.now() - start_time) / 200);
+    console.log(cur_frame);
+    if (cur_frame <= 3) {
+        map.setPaintProperty(id, "circle-radius", animation[cur_frame][0]);
+        map.setPaintProperty(id, "circle-opacity", animation[cur_frame][1]);
+        requestAnimationFrame(function() {
+            animatePoint(id);
+        });
+    } else {
+        map.removeSource(id);
+        map.removeLayer(id);
+    }
 }
